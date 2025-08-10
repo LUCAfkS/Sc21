248f120d1b81b10d05d32e9fb74aacbb66a83b00
Lucas <lucasrm004@gmail.com>
2025-08-10 20:48:12 -0300

    styles-back..

--- c21/backend/game-sys.py
+++ c21/backend/game-sys.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit, send
+from time import sleep
+from threading import Thread
 
 app = Flask(__name__, template_folder='../templates', static_folder='../styles',)
 socketio = SocketIO(app, cors_allowed_origins="*")  # Liberando CORS para testes locais
@@ -14,5 +16,29 @@ def handle_message(msg):
     send("Mensagem recebida no servidor!", broadcast=True)
 
 
+@socketio.on('times')
+def receber_tempo(valor):
+    print('Tempo recebido:', valor)
+    emit('resposta_tempo', f"Tempo recebido: {valor}")
+
+def timer():
+    s= 30
+    m= 1
+    first = True
+    while True:
+        s-=1
+        if s ==0 and m ==0:
+            socketio.emit('timer',str(m)+':0'+str(s)) 
+            break
+        elif s ==0:
+            s+=59
+            m-=1
+        if s < 10:
+            socketio.emit('timer',str(m)+':0'+str(s)) 
+        else:
+            socketio.emit('timer',str(m)+':'+str(s)) 
+        sleep(1)
+
 if __name__ == '__main__':
+    Thread(target=timer).start()
     socketio.run(app, debug=True)
